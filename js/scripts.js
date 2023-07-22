286b71e4178b38b00e3cc2bb26348de051ef433e
jenea130 <jenea130@mail.ru>
2023-07-22 19:35:06 +0300

    about: structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-    $('.header__lang select').styler();
+    $('.header__lang select').styler({
+        // onSelectOpened: function () {
+        //     // к открытому селекту добавляется красная обводка
+        //     // $(this).css('outline', '3px solid red');
+        //     setTimeout(() => {
+        //         $(".jq-selectbox__dropdown li").eq(0).css("visibility", "hidden");
+        //     }, 1000)
+
+        // }
+    });
 });
