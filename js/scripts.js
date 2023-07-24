72f7d8d3f5bbfd1bf0b81f5161d74d39e5ec0f06
jenea130 <jenea130@mail.ru>
2023-07-24 21:22:19 +0300

    letter: finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,5 @@
 $(document).ready(function () {
-    $('.header__lang select').styler({
-        // onSelectOpened: function () {
-        //     // к открытому селекту добавляется красная обводка
-        //     // $(this).css('outline', '3px solid red');
-        //     setTimeout(() => {
-        //         $(".jq-selectbox__dropdown li").eq(0).css("visibility", "hidden");
-        //     }, 1000)
-
-        // }
-    });
+    $('.header__lang select').styler();
     $('.home-intro__down').on('click', function (e) {
         e.preventDefault();
         const id = $(this).attr('href');
